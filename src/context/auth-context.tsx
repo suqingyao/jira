602e88b7e5bc2b1f195f4a7e6e1f0377c839d585
cullyfung <fcx.cli@qq.com>
2022-09-25 10:10:23 +0800

    feat: handle api:me error and loading

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -3,6 +3,8 @@ import { createContext, ReactNode, useContext, useState } from 'react'
 import { User } from '@/screens/project-list/search-panel'
 import { http } from '@/utils/http'
 import { useMount } from '@/utils'
+import { useAsync } from '@/utils/useAsync'
+import { FullPageErrorFallback, FullPageLoading } from '@/components/lib'
 
 interface AuthForm {
   username: string
@@ -32,15 +34,31 @@ const AuthContext = createContext<
 AuthContext.displayName = 'AuthContext'
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null)
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser
+  } = useAsync<User | null>()
 
   const login = (form: AuthForm) => auth.login(form).then(setUser)
   const register = (form: AuthForm) => auth.register(form).then(setUser)
   const logout = () => auth.logout().then(() => setUser(null))
 
   useMount(() => {
-    bootstrapUser().then(setUser)
+    run(bootstrapUser())
   })
+
+  if (isIdle || isLoading) {
+    return <FullPageLoading />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
 
   return (
     <AuthContext.Provider value={{ user, login, register, logout }}>
