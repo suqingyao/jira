f3251a971a35b6c56b9894cd4cc6cddb5d6567d0
cullyfung <fcx.cli@qq.com>
2022-10-04 20:45:20 +0800

    fix: query client bug

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import { AppProviders } from '@/context'
 import 'antd/dist/antd.less'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClientProvider, useQueryClient } from 'react-query'
 import App from './App'
 import './App.css'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
+const queryClient = useQueryClient()
 
 root.render(
   <React.StrictMode>
     <AppProviders>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </AppProviders>
   </React.StrictMode>
 )
